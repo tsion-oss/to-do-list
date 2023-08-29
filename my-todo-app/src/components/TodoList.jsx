23d60269c735fd8020c5f6fecb398e0463225859
Tsion <tsionwendmu12@gmail.com>
2023-08-29 14:36:55 -0400

    s

--- my-todo-app/src/components/TodoList.jsx
+++ my-todo-app/src/components/TodoList.jsx
@@ -3,19 +3,39 @@ import { useState } from "react"
 export default function TodoList () {
     const [ListItems, setListItems] = useState(['Bakery and Bread', 'Meat', 'Seafood', 'Pasta', 'Rice', 'Oils'])
 
+    const [newItem, setNewItem] = useState('')
+
+    console.log(newItem)
+
+   const handleChange = (e) => {
+     const value = e.target.value
+     setNewItem(value)
+   }
+
+   const handleSubmit = (e) => {
+    e.preventDefault()
+    if (newItem !== '') { 
+      setListItems([...ListItems, newItem])
+      setNewItem('')
+    }
+   }
+
     return (
      <div>
      
       <h2>Add your list of items</h2>
-      <form>
-        <input></input>
-        <button>Add</button>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="add your item"  onChange={handleChange}></input>
+        <button type="submit">Add</button>
       </form>
 
 
-       {ListItems.map((list) => (
-        <div>
-          <h2>{list}</h2>
+       {ListItems.slice().reverse().map((list) => (
+        <div >
+            <ul>
+               <li><h2>{list}</h2></li> 
+            </ul>
+          
         </div>
        ))}
      </div>
